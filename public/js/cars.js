7143125d0e8344e9fb5aaee28dc3d6a27086c2c1
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2018-06-09 23:15:47 +0000

    automatically backed up by learn

--- public/js/cars.js
+++ public/js/cars.js
@@ -15,7 +15,7 @@ var i = 0;
 $.each(carsJSON, function(index, car){
     html+= "<div class=\"col-md-4 car\">";
     html+='<h2>'+car.Make+"</h2>";
-    html+='<p><strong>Model:</strong> '+carsJSON[i]["Model"]+'</p>';
+    html+='<p><strong>Model:</strong> '+car.Model+'</p>';
     html+='<p><strong>Year:</strong> '+carsJSON[i]["Year"]+'</p>';
     html+='</div>';
     i++;
